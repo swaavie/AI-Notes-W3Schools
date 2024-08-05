828906e1b14df6cc745e796169548def5c9eb7c3
swaavie <51272769+swaavie@users.noreply.github.com>
2024-08-04 23:48:25 -0400

    Organizing, Lots of Python Notes.

---
+++ Python Notes/Operators/Python Logical Operators.py
@@ -0,0 +1,11 @@
+a = 1
+b = 2
+
+#This code returns True if both statements are true
+print(a < 5 and b > 1)
+
+#This code returns True if one of the statements is true
+print(a < 5 or b < 1)
+
+#This code reverses the result, returns False if the result is true
+print(not(a < 5 or b < 1))
